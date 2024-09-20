3d2b60e59d0bcb84752a969c7d6f6c4a138b6b5f
Qv1ko <vicgarmur947@gmail.com>
2024-09-20 13:17:13 +0200

    Corrección de ejercicios y clase sobre tipos de funciones

---
+++ Desarrollo Web en Entorno Cliente/Unidad1-Fundamentos_de_javascript/clase04/script.js
@@ -0,0 +1,12 @@
+// Declaración de función
+function suma(num1, num2) {
+    return num1 + num2;
+}
+
+// Expresión de función
+let suma = function (num1, num2) {
+    return num1 + num2;
+};
+
+// Función flecha
+let suma = (num1, num2) => num1 + num2;
--- Desarrollo Web en Entorno Cliente/Unidad1-Fundamentos_de_javascript/ejercicio01/ejercicio6.js
+++ Desarrollo Web en Entorno Cliente/Unidad1-Fundamentos_de_javascript/ejercicio01/ejercicio6.js
@@ -1,17 +1,43 @@
 console.log("--- Ejercicio 6 ---");
 
 let line = "#";
-let number = prompt("Escribe un número para la base del triangulo");
+triangle(prompt("Escribe un número para la base del triangulo"));
 
-if (number == "" || number == null) {
-    triangle();
-} else if (number > 0 && number % 1 == 0) {
-    triangle(number);
-}
-
+// Declaración de una función
 function triangle(number = 7) {
+    if (number == "" || number == null) {
+        number = 7;
+    } else if (!number > 0 && !number % 1 == 0) {
+        return;
+    }
     while (line.length <= number) {
         console.log(line);
         line += "#";
     }
 }
+
+// Expresión de una función
+// let triangle = function (number = 7) {
+//     if (number == "" || number == null) {
+//         number = 7;
+//     } else if (!number > 0 && !number % 1 == 0) {
+//         return;
+//     }
+//     while (line.length <= number) {
+//         console.log(line);
+//         line += "#";
+//     }
+// };
+
+// Función flecha
+// triangle = (number = 7) => {
+//     if (number == "" || number == null) {
+//         number = 7;
+//     } else if (!number > 0 && !number % 1 == 0) {
+//         return;
+//     }
+//     while (line.length <= number) {
+//         console.log(line);
+//         line += "#";
+//     }
+// };
