2e8cf94bcf2676b89cbcbd4aa7f1fcc3ef261939
Qv1ko <vicgarmur947@gmail.com>
2024-09-20 13:18:18 +0200

    Clase de implementación de PHP en HTML

---
+++ Desarrollo Web en Entorno Servidor/Unidad2/clase01/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    $num = 5;
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clase 2</title>
+</head>
+
+<body>
+
+    <b>Hola</b>
+    <?php echo '<b>compañeros</b> '?>
+    <b>¿Que tal?</b>
+    <?php echo '<p>Número: ' . $num .  '</p> '?>
+
+    <?php
+        $nombre = 'Pedro';
+        echo "Hola" , $nombre;
+        echo "Hola <br>" , $nombre;
+        echo "Hola " , "<b>" , $nombre , "</b>";
+        echo "Hola <b>$nombre</b>";
+        echo 'Hola <b>$nombre</b>';
+    ?>
+
+</body>
+
+</html>
