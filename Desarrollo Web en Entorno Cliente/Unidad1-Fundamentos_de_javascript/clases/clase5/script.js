af89f766c8f718eaa32325687d21ca70f02e1920
Qv1ko <vicgarmur947@gmail.com>
2024-09-23 19:47:36 +0200

    Clase: Switch, continue...

---
+++ Desarrollo Web en Entorno Cliente/Unidad1-Fundamentos_de_javascript/clases/clase5/script.js
@@ -0,0 +1,26 @@
+let a = +prompt("a?", "");
+
+switch (a) {
+    case 0:
+    case 1:
+        alert(a);
+        break;
+
+    case 2:
+    case 3:
+        alert("2,3");
+        break;
+
+    default:
+        break;
+}
+
+let edad = 18;
+
+edad == 18 && alert("Eres mayor de edad");
+
+// let v;
+
+// v != undefined || v == 5;
+
+// console.log(v);
