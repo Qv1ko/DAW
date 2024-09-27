90b64a362d908ebb3938c89a8dc4fbed8abbd6b2
Qv1ko <vicgarmur947@gmail.com>
2024-09-27 18:20:39 +0200

    Ejercicio 6

--- Desarrollo Web en Entorno Cliente/Unidad2-Objetos_y_tipos_de_datos/ejercicios/ejercicio6.js
+++ Desarrollo Web en Entorno Cliente/Unidad2-Objetos_y_tipos_de_datos/ejercicios/ejercicio6.js
@@ -2,14 +2,32 @@ console.log("--- Ejercicio 6 ---");
 
 calculator = {
     read: function () {
-        calculator.num1 = +prompt("Primer valor");
-        calculator.num2 = +prompt("Segundo valor");
+        let value;
+
+        do {
+            value = prompt("Primer valor");
+        } while (value == "" || value == null || isNaN(value));
+        calculator.num1 = Number(value);
+        do {
+            value = prompt("Segundo valor");
+        } while (value == "" || value == null || isNaN(value));
+        calculator.num2 = Number(value);
     },
     sum: function () {
         return (
-            (this.num1 in calculator ? this.num1 : 0) +
-            (this.num2 in calculator ? this.num2 : 0)
+            ("num1" in calculator ? this.num1 : 0) +
+            ("num2" in calculator ? this.num2 : 0)
         );
     },
-    mul: function () {},
+    mul: function () {
+        return (
+            ("num1" in calculator ? this.num1 : 0) *
+            ("num2" in calculator ? this.num2 : 0)
+        );
+    },
 };
+
+calculator.read();
+
+alert(calculator.sum());
+alert(calculator.mul());
