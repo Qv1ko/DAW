592611911134d9a74d01740a2c6e8bc071c15937
Qv1ko <vicgarmur947@gmail.com>
2024-09-30 16:48:04 +0200

    Clase sobre arrays y importación de código externo

---
+++ Desarrollo Web en Entorno Servidor/Unidad2-Elementos_base_de_php/clases/clase5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clase 5</title>
+</head>
+
+<body>
+    <?php
+
+        $aulasInformatica = ["10" => 20, "11" => 28, "12" => 24, "13" => 26, "14" => 24, "15" => 24, "25" => 24, "34" =>26];
+
+        // include.one('./maximos.php');
+        // requiere.one('./maximos.php');
+
+        // include ('./valores.inc');
+        // echo VALOR;
+
+        require ('./valores.inc');
+        echo VALOR;
+
+    ?>
+</body>
+
+</html>
