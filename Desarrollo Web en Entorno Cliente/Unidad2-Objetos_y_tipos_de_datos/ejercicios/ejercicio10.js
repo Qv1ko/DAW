3f2fb0cdb9bffdbac8a41405252b24c41d747bd8
Qv1ko <vicgarmur947@gmail.com>
2024-10-02 17:05:26 +0200

    Ejercicio JavaScript

---
+++ Desarrollo Web en Entorno Cliente/Unidad2-Objetos_y_tipos_de_datos/ejercicios/ejercicio10.js
@@ -0,0 +1,9 @@
+console.log("--- Ejercicio 10 ---");
+
+let number = +prompt("Escribe un número");
+
+console.log(number);
+console.log(number.toFixed(4));
+console.log(number.toString(2));
+console.log("0o" + number.toString(8));
+console.log("0x" + number.toString(16));
