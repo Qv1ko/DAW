0a07873f2dad497ecb6a131ba1bb93ab55e76d83
Qv1ko <vicgarmur947@gmail.com>
2024-10-12 22:30:17 +0200

    Ejercicio PHP

--- Desarrollo Web en Entorno Servidor/Unidad2-Elementos_base_de_php/ejercicios/ejercicios3-funciones/ejercicio1.php
+++ Desarrollo Web en Entorno Servidor/Unidad2-Elementos_base_de_php/ejercicios/ejercicios3-funciones/ejercicio1.php
@@ -10,13 +10,15 @@
 
 <body>
     <?php
-        
+
+        require_once('./ejercicio1-funciones.php');
+
         $numbers = array(rand(-17, 17), rand(-17, 17), rand(-17, 17), rand(-17, 17), rand(-17, 17));
 
         print_r($numbers);
         echo "<br><br>";
         echo "<p>Número de componentes negativos: " . componentesNegativos($numbers) . "</p>";
-        echo "<p>La media de los componentes es: " . componentesMedia($numbers) . "</p>";
+        echo "<p>La media de los componentes: " . componentesMedia($numbers) . "</p>";
         echo "<p>Posiciciones en las que se almacena como valor el cuadrado de su posición: " . valoresCuadradoPosicion($numbers) . "</p>";
         echo "<p>Array en orden invertido: " . arrayInvertido($numbers) . "</p>";
         echo "<p>Valores dobles del array: " . arrayDoble($numbers) . "</p>";
@@ -25,74 +27,10 @@
         echo "<p>Ordenación con rsort(): " . ordenarRsort($numbers) . "</p>";
         echo "<p>Resultado de la busqueda: " . buscar($numbers, "3") . "</p>";
         echo "<p>Resultado de la busqueda estricta: " . buscarEstricto($numbers, 7) . "</p>";
-
-        function componentesNegativos(array $numbers): int {
-
-            $count = 0;
-
-            foreach ($numbers as $number) {
-                if ($number < 0) {
-                    $count++;
-                }
-            }
-
-            return $count;
-
-        }
-
-        function componentesMedia(array $numbers): float {
-            return array_sum($numbers) / count($numbers);
-        }
-
-        function valoresCuadradoPosicion(array $numbers): string {
-
-            $text = "";
-
-            for ($i = 0; $i < count($numbers); $i++) {
-                for ($j = 0; $j < count($numbers); $j++) {
-                    if ($i * $i == $numbers[$j]) {
-                        $text = $text . ($text != "" ? ", " : "") . "[" . strval($j) . "] = " . $numbers[$j];
-                    }
-                }
-            }
-
-            return $text != "" ? $text : "No se da la circunstancia";
-
-        }
-
-        function arrayInvertido(array $numbers): string {
-            return implode(", ", array_reverse($numbers));
-        }
-
-        function arrayDoble(array $numbers): string {
-            return implode(", ", array_map(fn(int $number): int => $number * 2, $numbers));
-        }
-
-        function arrayMitad(array $numbers): string {
-            return implode(", ", array_map(fn(int $number): float => $number / 2, $numbers));
-        }
-
-        function ordenarSort(array &$numbers): string {
-            sort($numbers);
-            array_push($numbers, rand(-17, 17));
-            sort($numbers);
-            return implode(", ", $numbers);
-        }
-
-        function ordenarRsort(array &$numbers): string {
-            rsort($numbers);
-            array_push($numbers, rand(-17, 17));
-            rsort($numbers);
-            return implode(", ", $numbers);
-        }
-
-        function buscar(array $numbers, mixed $value): string {
-            return "El valor $value" . (in_array($value, $numbers) ? " se a encontrado" : " no se a encontrado");
-        }
-
-        function buscarEstricto(array $numbers, mixed $value): string {
-            return "El valor $value" . (!is_bool(array_search($value, $numbers, true)) ? " se a encontrado en la posición " . array_search($value, $numbers, true) . " del array": " no se a encontrado");
-        }
+        incrementarDos($numbers);
+        echo "<p>La media de los componentes despues de incrementarlos en dos: " . componentesMedia($numbers) . "</p>";
+        borrarUltimoValor($numbers);
+        echo "<p>Array despues de borrar el ultimo valor: " . implode(", ", $numbers) . "</p>";
 
     ?>
 </body>
