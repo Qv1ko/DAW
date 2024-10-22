69a03705897bf1e1f2f2e5cc6aa451711029c20c
Qv1ko <vicgarmur947@gmail.com>
2024-10-22 16:31:53 +0200

    Corrección de la estuctura de JavaScript

--- Desarrollo Web en Entorno Cliente/Unidad2-Objetos_y_tipos_de_datos/clases/clase3.js
+++ Desarrollo Web en Entorno Cliente/Unidad2-Objetos_y_tipos_de_datos/clases/clase3.js
@@ -1,3 +1,28 @@
+function Persona(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.saluda = function () {
+        console.log(`Hola, soy ${this.name} ${this.apellido}`);
+    };
+    this.asignarEdad = function (edad) {
+        this.edad = edad;
+    };
+    this.toString = function () {
+        return this.nombre + " " + this.apellido;
+    };
+    this.valueOf = function () {
+        return this.edad;
+    };
+}
+
+let pepe = new Persona("Pepe", "Pérez");
+
+pepe.saluda();
+
+pepe.asignarEdad(40);
+
+alert(pepe);
+
 let nombre = "jose";
 
 alert(nombre.toUpperCase());
