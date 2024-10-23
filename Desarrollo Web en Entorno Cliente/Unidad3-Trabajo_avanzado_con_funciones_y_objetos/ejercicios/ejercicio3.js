de6dc5608470733f388a4dbeedfd1f6dfcc33c0b
Qv1ko <vicgarmur947@gmail.com>
2024-10-23 18:28:12 +0200

    Ejercicio JavaScript

---
+++ Desarrollo Web en Entorno Cliente/Unidad3-Trabajo_avanzado_con_funciones_y_objetos/ejercicios/ejercicio3.js
@@ -0,0 +1,16 @@
+console.log("--- Ejercicio 3 ---");
+
+/*
+ * Nos gustaría obtener un array de map.keys() en una variable y luego aplicarle métodos específicos de arrays.
+ */
+
+let map = new Map();
+
+map.set("name", "John");
+
+//let keys = map.keys(); // map.keys() no devuelve un array, si no un iterador
+//keys.push("more"); // Error: keys.push no es una función
+
+let keys = Array.from(map.keys());
+
+keys.push("more");
