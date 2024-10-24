891ba05d2dbff3d54c1882b6dec39ad68f002e37
Qv1ko <vicgarmur947@gmail.com>
2024-10-24 17:37:46 +0200

    Corrección de ejercicios

--- Desarrollo Web en Entorno Servidor/Unidad2-Elementos_base_de_php/ejercicios/ejercicios4-poo/ejercicio3_opcion2-primero.php
+++ Desarrollo Web en Entorno Servidor/Unidad2-Elementos_base_de_php/ejercicios/ejercicios4-poo/ejercicio3_opcion2-primero.php
@@ -13,6 +13,20 @@
             return array_sum($this->clasificaciones) / count($this->clasificaciones);
         }
 
+        public function calcular_media(): float {
+
+            $media = 0;
+            $iterador = new ArrayIterator($this->clasificaciones);
+
+            for ($i = 0; $i < $iterador->count(); $i++) { 
+                $media += $iterador->current();
+                $iterador->next();
+            }
+
+            return $media / count($this->clasificaciones);
+
+        }
+
         public function supera_curso(mixed &$resultado): string {
             $resultado = $this->nota_media() >= 5;
             return "<p>El alumno $this->nombre" . ($this->nota_media() >= 5 ? " " : " no ") . "ha aprobado el primer curso</p>";
