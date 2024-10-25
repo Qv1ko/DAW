195a92cc7e403084b990db2f862bc4230c0017e4
Qv1ko <vicgarmur947@gmail.com>
2024-10-25 16:20:22 +0200

    Ejercicios JavaScript

--- Desarrollo Web en Entorno Cliente/Unidad3-Trabajo_avanzado_con_funciones_y_objetos/ejercicios/ejercicio21.js
+++ Desarrollo Web en Entorno Cliente/Unidad3-Trabajo_avanzado_con_funciones_y_objetos/ejercicios/ejercicio21.js
@@ -5,3 +5,34 @@ let cadenaJSON = "[1, 2, 3, 2, 4, 6, 1]";
 let arr = JSON.parse(cadenaJSON).map((number) => number * 2);
 
 console.log(arr);
+
+fetch("./ejercicio21.json")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("La respuesta no es correcta");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        let pokemons = data;
+        pokemons.pokemon.forEach((pokemon) => {
+            console.log(`ID: ${pokemon.id}`);
+            console.log(`Nombre: ${pokemon.name}`);
+            console.log(`Tipo: ${pokemon.type.join(", ")}`);
+            console.log(`Debilidades: ${pokemon.weaknesses.join(", ")}`);
+            console.log(
+                `Evoluciones: ${
+                    pokemon.next_evolution
+                        ? pokemon.next_evolution.map((e) => e.nombre).join(", ")
+                        : "Ninguna"
+                }`
+            );
+            console.log("-------------------");
+        });
+    })
+    .catch((error) => {
+        console.error(
+            "Hubo un problema con la operación de obtención: ",
+            error
+        );
+    });
