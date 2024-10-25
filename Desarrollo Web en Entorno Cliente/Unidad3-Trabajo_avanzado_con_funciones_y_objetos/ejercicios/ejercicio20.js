c73528bf3d0b8bbbf6f0fc7e8370eb28478e0a41
Qv1ko <vicgarmur947@gmail.com>
2024-10-25 16:05:48 +0200

    Corrección de ejercicios

--- Desarrollo Web en Entorno Cliente/Unidad3-Trabajo_avanzado_con_funciones_y_objetos/ejercicios/ejercicio20.js
+++ Desarrollo Web en Entorno Cliente/Unidad3-Trabajo_avanzado_con_funciones_y_objetos/ejercicios/ejercicio20.js
@@ -4,19 +4,19 @@ let alum1 = { nombre: "Fede", edad: 30, estudios: ["SMR"] };
 let alum2 = { nombre: "Sol", edad: 25, estudios: [] };
 let clase = ["DWEC", 126, alum1, alum2];
 
-let copia = JSON.stringify(clase);
+let copia = JSON.parse(JSON.stringify(clase));
 
 alum1.matricula = "331J";
 
-console.log(JSON.parse(copia)[2]);
+console.log(copia[2]);
 console.log(alum1);
 
 alum2.matricula = "884U";
 
-console.log(JSON.parse(copia)[3]);
+console.log(copia[3]);
 console.log(alum2);
 
 clase.push(92);
 
-console.log(JSON.parse(copia));
+console.log(copia);
 console.log(clase);
