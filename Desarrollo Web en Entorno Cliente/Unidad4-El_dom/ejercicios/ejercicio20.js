156baf8a0cb88381c60422df8622e87cd1620470
Qv1ko <vicgarmur947@gmail.com>
2024-11-14 13:55:11 +0100

    Ejercicios JavaScript

---
+++ Desarrollo Web en Entorno Cliente/Unidad4-El_dom/ejercicios/ejercicio20.js
@@ -0,0 +1,9 @@
+console.log("--- Ejercicio 20 ---");
+
+/*
+ * Escribe el código para insertar <li>2</li><li>3</li> entre dos <li>
+ */
+
+ul = document.getElementById("ul");
+
+ul.childNodes[1].insertAdjacentHTML("afterend", "<li>2</li><li>3</li> ");
---
+++ Desarrollo Web en Entorno Cliente/Unidad4-El_dom/ejercicios/ejercicio21.js
@@ -0,0 +1,36 @@
+console.log("--- Ejercicio 21 ---");
+
+/*
+ * Escribe el código para ordenar la tabla por la columna "name".
+ */
+
+ordenedTable(document.querySelectorAll("table")[0], "name");
+
+function ordenedTable(table, columnName) {
+    let headerRow = table.getElementsByTagName("th");
+    let column = 0;
+
+    for (let i = 0; i < headerRow.length; i++) {
+        if (
+            headerRow[i].textContent.toLowerCase() == columnName.toLowerCase()
+        ) {
+            column = i;
+            break;
+        }
+    }
+
+    rows = Array.from(document.querySelectorAll("table tbody tr"));
+
+    rows.sort(
+        (row1, row2) =>
+            row1.children[column].textContent >
+            row2.children[column].textContent
+    );
+
+    tb = document.querySelector("table tbody");
+    tb.innerHTML = "";
+
+    rows.forEach((row) => {
+        tb.append(row);
+    });
+}
