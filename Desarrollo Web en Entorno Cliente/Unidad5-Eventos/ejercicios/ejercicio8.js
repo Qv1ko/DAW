3620f9735f6e46106f59c5ae34ec2d5e66cee7a4
Qv1ko <vicgarmur947@gmail.com>
2024-11-25 17:12:48 +0100

    Ejercicios de JavaScript

---
+++ Desarrollo Web en Entorno Cliente/Unidad5-Eventos/ejercicios/ejercicio8.js
@@ -0,0 +1,27 @@
+console.log("--- Ejercicio 8 ---");
+
+/*
+ * Se pide que al hacer clic sobre un texto, si tiene nodos hijos, los
+ *  muestre u oculte.
+ * La solución tiene dos partes:
+ *  - Envuelve cada nodo de texto del árbol dentro de <span>.
+ *  - Establece el manejador al nodo raíz del árbol y maneja los clics
+ *     en aquellos nodos que sean  <span>.
+ */
+
+// Solución sin delegación de eventos
+
+treeItems = document.querySelectorAll("#tree li");
+
+for (let li of treeItems) {
+    span = document.createElement("span");
+    span.textContent = li.firstChild.data.trim();
+    span.addEventListener("click", (e) => {
+        const SUBUL = e.target.parentElement.childNodes[1];
+        if (SUBUL) {
+            SUBUL.hidden = !SUBUL.hidden;
+        }
+    });
+    li.firstChild.remove();
+    li.prepend(span);
+}
