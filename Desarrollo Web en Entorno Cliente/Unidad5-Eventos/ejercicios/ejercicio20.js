4f1e9379ff54e41604ff4c6717cb5056f49a0029
Qv1ko <vicgarmur947@gmail.com>
2024-12-07 18:25:01 +0100

    Ejercicio JavaScript

--- Desarrollo Web en Entorno Cliente/Unidad5-Eventos/ejercicios/ejercicio20.js
+++ Desarrollo Web en Entorno Cliente/Unidad5-Eventos/ejercicios/ejercicio20.js
@@ -12,7 +12,6 @@ console.log("--- Ejercicio 20 ---");
  * - Si las dos imágenes son diferentes, se ocultarán mostrando nuevamente el color inicial.
  * - Cuando se hayan completado las 6 parejas, se mostrará el mensaje: ¡¡GANASTE!!
  * - El botón de reiniciar el juego, cambia el orden de las cartas para volver a jugar.
- * El siguiente método te puede ayudar, permite ordenar un array aleatoriamente: array.sort(() => 0.5 - Math.random());
  */
 
 document.addEventListener("DOMContentLoaded", game);
@@ -25,63 +24,83 @@ const CHARACTERS = [
     "./img/maggie.png",
     "./img/marge.png",
 ];
-let chars = mixCharacters(CHARACTERS);
+let isProcessing = false;
 
 function game() {
+    let chars = mixCharacters(CHARACTERS);
     let table = document.createElement("table");
     const ROWS = 3;
-    const COLUMNS = chars.length / ROWS;
+    const COLUMNS = Math.ceil(chars.length / ROWS);
     let row, cell;
+    const COLOR = "#c0a902";
     let viewCards = [];
+    let marker = 0;
 
     document.body.innerHTML = "";
+
+    let markerText = document.createElement("p");
+    markerText.textContent = "Marcador: " + marker;
+    document.body.appendChild(markerText);
+
+    let restartButton = document.createElement("button");
+    restartButton.textContent = "Reiniciar el juego";
+    restartButton.style.marginBottom = "16px";
+    restartButton.addEventListener("click", game);
+    document.body.appendChild(restartButton);
 
     for (let r = 0; r < ROWS; r++) {
         row = document.createElement("tr");
         for (let c = 0; c < COLUMNS; c++) {
             cell = document.createElement("td");
-
             cell.style.width = "218px";
             cell.style.height = "230px";
-            cell.style.backgroundColor = randColor();
+            cell.style.backgroundColor = COLOR;
 
             cell.addEventListener("click", function clickEvent(e) {
-                // this.querySelectorAll("td").forEach(
-                //     (td) => (td.style.pointerEvent = "none")
-                // );
-                if (
-                    e.target.style.backgroundImage == "" &&
-                    viewCards.length < 2
-                ) {
-                    e.target.style.backgroundImage =
-                        "url('" + chars[r * COLUMNS + c] + "')";
-                    viewCards.push(e.target);
-                } else {
+                if (isProcessing) return;
+                if (e.target.style.backgroundImage) {
                     e.target.style.backgroundImage = "";
-                    viewCards = viewCards.filter(
-                        (card) =>
-                            card.style.backgroundImage !=
-                            e.target.backgroundImage
-                    );
+                    return;
                 }
-                if (viewCards.length == 2) {
-                    let card1 = viewCards[0];
-                    let card2 = viewCards[1];
+
+                e.target.style.backgroundColor = "";
+                e.target.style.backgroundImage = `url('${
+                    chars[r * COLUMNS + c]
+                }')`;
+                viewCards.push(e.target);
+
+                if (viewCards.length === 2) {
+                    isProcessing = true;
+
+                    const [card1, card2] = viewCards;
+
                     if (
-                        card1.style.backgroundImage ==
+                        card1.style.backgroundImage ===
                             card2.style.backgroundImage &&
-                        card1 != card2
+                        card1 !== card2
                     ) {
                         viewCards.forEach((card) =>
                             card.removeEventListener("click", clickEvent)
                         );
+                        viewCards = [];
+                        marker++;
+                        markerText.textContent = "Marcador: " + marker;
+                        isProcessing = false;
+                        if (marker == 6) {
+                            setTimeout(() => {
+                                alert("¡¡GANASTE!!");
+                            }, 33);
+                        }
                     } else {
                         setTimeout(() => {
                             card1.style.backgroundImage = "";
+                            card1.style.backgroundColor = COLOR;
                             card2.style.backgroundImage = "";
+                            card2.style.backgroundColor = COLOR;
+                            viewCards = [];
+                            isProcessing = false;
                         }, 500);
                     }
-                    viewCards.length = 0;
                 }
             });
 
@@ -97,15 +116,4 @@ function mixCharacters(characters) {
     return characters
         .concat(characters.sort(() => 0.5 - Math.random()))
         .sort(() => 0.5 - Math.random());
-}
-
-function randColor() {
-    const DIGITS = 6;
-    let color = "#";
-
-    for (let i = 0; i < DIGITS; i++) {
-        color += Math.ceil(Math.random() * 15).toString(16);
-    }
-
-    return color;
 }
