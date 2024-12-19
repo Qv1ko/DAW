36437f0d625ea77b4f93fd3f617dc6972368cc9f
Qv1ko <vicgarmur947@gmail.com>
2024-12-19 17:34:10 +0100

    Clase de localStorage

---
+++ Desarrollo Web en Entorno Cliente/Unidad6-formularios/clases/js/clase3.js
@@ -0,0 +1,11 @@
+console.log("--- Clase 3 ---");
+
+localStorage.setItem("test", 1);
+console.log(localStorage.getItem("test"));
+
+document.cookie = "cookie=micookie";
+console.log(document.cookie);
+
+localStorage.setItem("valor", "cadena-simple");
+
+sessionStorage.setItem("dato", "dato de sesión");
