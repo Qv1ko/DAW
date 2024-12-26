d0add6f18afad34044ca15eea150c89973d084aa
Qv1ko <vicgarmur947@gmail.com>
2024-12-26 19:10:48 +0100

    Añadida variables para el calculo del descuento

--- Desarrollo Web en Entorno Servidor/Unidad3-Gestion_de_los_formularios_y_enlaces/examen/datos_aux.php
+++ Desarrollo Web en Entorno Servidor/Unidad3-Gestion_de_los_formularios_y_enlaces/examen/datos_aux.php
@@ -2,9 +2,12 @@
 
     require_once("./f_victorg1.php");
 
+    const GENERO_DESCONTADO = "Aventuras";
+
     $libros = construirArray();
     $mensajePrecio = "";
     $libro;
+    $descuento = 0;
     $resultado = "";
 
 ?>
