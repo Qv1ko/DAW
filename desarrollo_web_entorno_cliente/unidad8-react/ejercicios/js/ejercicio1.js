64792ba9298004c4eb3dc4a9661bcf7c74eafca5
Qv1ko <vicgarmur947@gmail.com>
2025-02-07 15:05:55 +0100

    Ejercicio JavaScript

---
+++ desarrollo_web_entorno_cliente/unidad8-react/ejercicios/js/ejercicio1.js
@@ -0,0 +1,9 @@
+import { loadContent } from "../modules/ejercicio1-evento.js";
+
+console.log("--- Ejercicio 1 ---");
+
+/*
+ * Realiza el ejercicio 14 de la unidad 7 usando módulos.
+ */
+
+loadContent();
